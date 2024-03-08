a8baebe0583f31bd533407df289f35aa04d409e1
nidhikumari817 <dnidhi8178@gmail.com>
2024-03-08 15:30:19 +0530

    SearchFunctionality

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.zara.com/in/");
-
-
+        Thread.sleep(2000);
+        driver.manage().window().fullscreen();
+        Thread.sleep(2000);
+        Search s= new Search();
+        s.SearchBar(driver);
     }
 }
