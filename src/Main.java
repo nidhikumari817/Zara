e734091ed3053ff36d75309cd98f8d0485d6189e
nidhikumari817 <dnidhi8178@gmail.com>
2024-03-08 13:12:54 +0530

    Zara

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.zara.com/in/");
+
+
+    }
+}
